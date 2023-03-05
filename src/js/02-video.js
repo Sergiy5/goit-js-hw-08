123c4e8c3008b4f8e78c1d757b197c61ea0f4191
Sergiy Kushnir <s1983kushnir@gmail.com>
2023-03-05 19:40:05 +0200

    Fix ex 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,6 +3,10 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const plaeyrCurrentTime = localStorage.getItem('videoplayer-current-time');
+const parsPlaeyrCurrentTime = JSON.parse(plaeyrCurrentTime);
+
+playerStartTime();
 
   player.on(
     'timeupdate',
@@ -12,14 +16,14 @@ const player = new Player(iframe);
     
   );
 
-    const plaeyrCurrentTime = localStorage.getItem('videoplayer-current-time');
-    const parsPlaeyrCurrentTime = JSON.parse(plaeyrCurrentTime);
-    const dataTime = parsPlaeyrCurrentTime.seconds;
- 
+function playerStartTime() {
 
+  if (plaeyrCurrentTime) {
+    const dataTime = parsPlaeyrCurrentTime.seconds;
+    
     player
       .setCurrentTime(dataTime)
-      .then(function () {
+      .then(function (seconds) {
         // seconds = the actual time that the player seeked to
       })
       .catch(function (error) {
@@ -33,4 +37,5 @@ const player = new Player(iframe);
             break;
         }
       });
-
+  }
+};
